36316af223d59ec4a74ce1edb501f13f99936c1a
misael-reyes <misaelstark11@gmail.com>
2023-07-08 11:44:29 -0600

    init project

---
+++ app/src/main/java/com/developersunited/geolocationtracker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.developersunited.geolocationtracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
