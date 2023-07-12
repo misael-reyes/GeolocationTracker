59a9d59e07f286db97702f3157d8d38ffafca9b5
misael-reyes <misaelstark11@gmail.com>
2023-07-12 11:27:39 -0600

    estructura inicial carpetas

--- app/src/main/java/com/developersunited/geolocationtracker/ui/MainActivity.kt
+++ app/src/main/java/com/developersunited/geolocationtracker/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.developersunited.geolocationtracker
+package com.developersunited.geolocationtracker.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.developersunited.geolocationtracker.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
