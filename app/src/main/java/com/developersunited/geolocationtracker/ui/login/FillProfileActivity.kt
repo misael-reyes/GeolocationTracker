e0dde985b97464577be53c7df7e39ee194922164
MarkosCe <cesar.markos99@gmail.com>
2023-07-13 17:37:36 -0600

    lets view and fill profile view

---
+++ app/src/main/java/com/developersunited/geolocationtracker/ui/login/FillProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.developersunited.geolocationtracker.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.developersunited.geolocationtracker.R
+
+class FillProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fill_profile)
+    }
+}
---
+++ app/src/main/java/com/developersunited/geolocationtracker/ui/login/LetsActivity.kt
@@ -0,0 +1,12 @@
+package com.developersunited.geolocationtracker.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.developersunited.geolocationtracker.R
+
+class LetsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lets)
+    }
+}
