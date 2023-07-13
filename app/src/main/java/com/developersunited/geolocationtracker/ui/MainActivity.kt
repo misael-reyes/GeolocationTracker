500ebedd48b5dde0563c1dc9eb20de2215b0e7d5
misael-reyes <misaelstark11@gmail.com>
2023-07-13 16:44:46 -0600

    vistas create y login account

--- app/src/main/java/com/developersunited/geolocationtracker/ui/MainActivity.kt
+++ app/src/main/java/com/developersunited/geolocationtracker/ui/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.developersunited.geolocationtracker.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.developersunited.geolocationtracker.R
+import com.developersunited.geolocationtracker.ui.login.CreateAccountActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, CreateAccountActivity::class.java))
     }
 }
