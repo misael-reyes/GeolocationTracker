d0a2edcc98e78c2a6bd29221c2acfb25694fae35
MarkosCe <cesar.markos99@gmail.com>
2023-07-15 22:41:02 -0600

    fix: values

--- app/src/main/java/com/developersunited/geolocationtracker/ui/login/LetsActivity.kt
+++ app/src/main/java/com/developersunited/geolocationtracker/ui/login/LetsActivity.kt
@@ -1,12 +1,28 @@
 package com.developersunited.geolocationtracker.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.developersunited.geolocationtracker.R
+import com.developersunited.geolocationtracker.databinding.ActivityLetsBinding
 
 class LetsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLetsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lets)
+        binding = ActivityLetsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.tvLetsSignUp.setOnClickListener {
+            startActivity(Intent(this, CreateAccountActivity::class.java))
+        }
+        binding.btnSignEmailPassword.setOnClickListener {
+            startActivity(Intent(this, LoginAccountActivity::class.java))
+        }
     }
 }
